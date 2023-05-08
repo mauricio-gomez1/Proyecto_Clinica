5f86692fba85a31006b6d6902f179e94f1bb2b20
mauricio-gomez1 <mauricio.gomez2710@gmail.com>
2023-05-08 02:36:58 -0600

    ssi se puede

--- assets/conn/dbconnect.php
+++ assets/conn/dbconnect.php
@@ -46,9 +46,9 @@
 ?>
 <?php
 
-$conn = mysqli_init();
+$con = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'dbhealthcare.mysql.database.azure.com', 'root1', 'Mauricio_1', 'db_healthcare', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, 'dbhealthcare.mysql.database.azure.com', 'root1', 'Mauricio_1', 'db_healthcare', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
