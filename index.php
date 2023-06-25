2f3fe4abdc7b6f4e2a09ec19f73516e876cec021
mauricio-gomez1 <mauricio.gomez2710@gmail.com>
2023-06-25 01:20:40 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
 include_once 'assets/conn/dbconnect.php';
 ?>
+
 <?php
 session_start();
+
+if (isset($_SESSION['patientSession'])) {
+    header("Location: patient/patient.php");
+}
+
 if (isset($_POST['login'])) {
     $icPatient = mysqli_real_escape_string($con, $_POST['icPatient']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
 
     $res = mysqli_query($con, "SELECT * FROM patient WHERE icPatient = '$icPatient'");
     $row = mysqli_fetch_array($res, MYSQLI_ASSOC);
-
+    
     if ($row['password'] == $password) {
         $_SESSION['patientSession'] = $row['icPatient'];
+
         header("Location: /patient/patient.php");
-        exit; // Asegúrate de usar exit después de la redirección
+        exit;
     } else {
         echo '<script>';
         echo 'alert("Wrong input");';
@@ -21,6 +28,7 @@ if (isset($_POST['login'])) {
     }
 }
 ?>
+
 <?php
 if (isset($_POST['signup'])) {
     $patientFirstName = mysqli_real_escape_string($con, $_POST['patientFirstName']);
@@ -36,15 +44,15 @@ if (isset($_POST['signup'])) {
 
     $query = "INSERT INTO patient (icPatient, password, patientFirstName, patientLastName, patientDOB, patientGender, patientEmail)
               VALUES ('$icPatient', '$password', '$patientFirstName', '$patientLastName', '$patientDOB', '$patientGender', '$patientEmail')";
-
+    
     $result = mysqli_query($con, $query);
 
     if ($result) {
-        echo '<script>';
+        echo '<script type="text/javascript">';
         echo 'alert("Register success. Please Login to make an appointment.");';
         echo '</script>';
     } else {
-        echo '<script>';
+        echo '<script type="text/javascript">';
         echo 'alert("User already registered. Please try again.");';
         echo '</script>';
     }
