dff15206039d5ee1c4030679cb0f8ca1e6639591
mauricio-gomez1 <mauricio.gomez2710@gmail.com>
2023-06-26 00:37:59 -0600

    Update profile.php

--- patient/profile.php
+++ patient/profile.php
@@ -87,7 +87,7 @@ $widowed = "checked";
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="patient.php"><img alt="Brand" src="assets/img/logo.png" height="40px"></a>
+					<a class="navbar-brand" href="patient.php"><img alt="Brand" src="assets/img/logoddd.png" height="40px"></a>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
