238a5e242e6da72de02084e8269b4520ccdf4d9c
mauricio-gomez1 <mauricio.gomez2710@gmail.com>
2023-06-26 00:17:20 -0600

    update img

--- patient/profile.php
+++ patient/profile.php
@@ -130,7 +130,7 @@ $widowed = "checked";
 						<div class="col-md-3 col-sm-3">
 							
 							<div class="user-wrapper">
-								<img src="assets/img/1.jpg" class="img-responsive" />
+								<img src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg" class="img-responsive" />
 								<div class="description">
 									<h4><?php echo $userRow['patientFirstName']; ?> <?php echo $userRow['patientLastName']; ?></h4>
 									<p>
