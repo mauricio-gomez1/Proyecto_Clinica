9162dc4fad775f62773529599db671eabade6f50
mauricio-gomez1 <mauricio.gomez2710@gmail.com>
2023-06-29 00:43:16 -0600

    Update cancelAppointment.php

--- patient/cancelAppointment.php
+++ patient/cancelAppointment.php
@@ -5,17 +5,30 @@ include_once '../assets/conn/dbconnect.php';
 if (isset($_GET['appid'])) {
     $appId = $_GET['appid'];
 
-    // Realiza la lógica para cancelar la cita y hacerla disponible nuevamente
-    // Por ejemplo, puedes ejecutar una consulta SQL para actualizar el estado de la cita
-
-    $query = "UPDATE appointment SET status = 'Disponible' WHERE appId = $appId";
+    // Obtén la información de la cita antes de eliminarla
+    $query = "SELECT * FROM appointment WHERE appId = $appId";
     $result = mysqli_query($con, $query);
+    $appointment = mysqli_fetch_array($result);
 
-    if ($result) {
-        // La cita se canceló correctamente, redirecciona al usuario a la página de citas
-        header("Location: patientapplist.php?patientId=".$_SESSION['patientSession']);
+    // Elimina la cita de la tabla appointment
+    $deleteQuery = "DELETE FROM appointment WHERE appId = $appId";
+    $deleteResult = mysqli_query($con, $deleteQuery);
+
+    if ($deleteResult) {
+        // Actualiza el campo bookAvail en la tabla doctorschedule
+        $scheduleId = $appointment['scheduleId'];
+        $updateQuery = "UPDATE doctorschedule SET bookAvail = 'available' WHERE scheduleId = $scheduleId";
+        $updateResult = mysqli_query($con, $updateQuery);
+
+        if ($updateResult) {
+            // La cita se canceló correctamente y el campo bookAvail se actualizó, redirecciona al usuario a la página de citas
+            header("Location: patientapplist.php?patientId=".$_SESSION['patientSession']);
+        } else {
+            // Ocurrió un error al actualizar el campo bookAvail, puedes mostrar un mensaje de error o realizar alguna otra acción
+            echo "Error al actualizar el campo bookAvail.";
+        }
     } else {
-        // Ocurrió un error al cancelar la cita, puedes mostrar un mensaje de error o realizar alguna otra acción
+        // Ocurrió un error al eliminar la cita, puedes mostrar un mensaje de error o realizar alguna otra acción
         echo "Error al cancelar la cita.";
     }
 }
