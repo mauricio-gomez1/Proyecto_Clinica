f920e14d3ed900aaae5a42419fac1f99fcd259a8
mauricio-gomez1 <mauricio.gomez2710@gmail.com>
2023-06-29 21:59:44 -0600

    Update profile.php

--- patient/profile.php
+++ patient/profile.php
@@ -256,7 +256,7 @@ $widowed = "checked";
 																		<i class="fa fa-calendar">
 																		</i>
 																	</div>
-																	<input class="form-control" id="patientDOB" name="patientDOB" placeholder="YYYY/MM/DD" type="text" value="<?php echo $userRow['patientDOB']; ?>"disabled/>
+																	<input class="form-control" id="patientDOB" name="patientDOB" placeholder="YYYY/MM/DD" type="text" value="<?php echo $userRow['patientDOB']; ?>"/>
 																</div>
 															</div>
 														</td>
@@ -267,10 +267,10 @@ $widowed = "checked";
 														<td>Genero</td>
 														<td>
 															<div class="radio">
-																<label><input type="radio" name="patientGender" value="male" <?php echo $male; ?> disabled>Hombre</label>
+																<label><input type="radio" name="patientGender" value="male" <?php echo $male; ?>>Hombre</label>
 															</div>
 															<div class="radio">
-																<label><input type="radio" name="patientGender" value="female" <?php echo $female; ?> disabled>Mujer</label>
+																<label><input type="radio" name="patientGender" value="female" <?php echo $female; ?>>Mujer</label>
 															</div>
 														</td>
 													</tr>
